45eca3164cb6132c56be5b1c0c95d8883ddfec4d
Celia Pennington <celiajpennington@gmail.com>
2021-08-26 16:38:43 -0500

    added schemas folder and index.js

--- server/server.js
+++ server/server.js
@@ -3,13 +3,14 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 const { ApolloServer } = require('apollo-server-express');
-
+const {authMiddleware} = require("./utils/auth.js")
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware,
 });
 //Middleware
 server.applyMiddleware({ app });
