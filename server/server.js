b6b7d78b8f960964a4a928850c70b81d4502a667
Celia Pennington <celiajpennington@gmail.com>
2021-08-26 17:18:43 -0500

    commented out routes and controllers

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+// const routes = require('./routes');
 const { ApolloServer } = require('apollo-server-express');
 const {authMiddleware} = require("./utils/auth.js")
 const app = express();
@@ -23,7 +23,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// app.use(routes);
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
